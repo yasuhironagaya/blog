d95df1e5a3a3f1b0b5d692746e133c4af1eb8407
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2023-01-09 20:39:03 +0900

    ♻️ ヘッダーにcssを当てました

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -2,12 +2,15 @@
 
 import { Logo } from '../logo'
 import { Nav } from '../nav'
+import styles from './header.module.scss'
 
 export function Header() {
   return (
     <header>
-      <Logo boxOn/>
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
   )
 }
