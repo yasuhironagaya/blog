89fa9448502d63b49e3ab92f6d9b6c8435f7b107
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2023-01-10 05:17:06 +0900

    ♻️ footerのレイアウト修正

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,11 +1,15 @@
 // footer
 
 import { Logo } from '../logo'
+import styles from './footer.module.scss'
 
 export function Footer() {
   return (
-    <footer>
-      <Logo />
+    <footer className={styles.wrapper}>
+      <div className={styles.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
     </footer>
   )
 }
