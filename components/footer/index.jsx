a053a5254ec3bd574ec754d4ecb218d41e8415e6
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2023-01-25 08:56:02 +0900

    133ページまで

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -4,7 +4,6 @@ import { Social } from 'components/social'
 import { Container } from 'components/container'
 import { Logo } from 'components/logo'
 import classes from './footer.module.scss'
-import { Posts } from 'components/posts'
 
 export function Footer() {
   return (
@@ -13,7 +12,6 @@ export function Footer() {
         <div className={classes.flexContainer}>
           <Logo />
           <Social />
-
         </div>
       </Container>
     </footer>
