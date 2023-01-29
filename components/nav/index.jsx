d7c0124e54e79a956e7f60cfa3d1773fa4ba1511
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2023-01-29 17:43:57 +0900

    ✨ メニュをﾓﾊﾞｲﾙに対応

--- components/nav/index.jsx
+++ components/nav/index.jsx
@@ -1,11 +1,18 @@
 // nav.jsx
 
+import { useState } from 'react'
 import Link from 'next/link'
 import classes from './nav.module.scss'
 
 export function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev)
+  }
   return (
-    <nav>
+    <nav className={navIsOpen ? classes.open : classes.close}>
+      <button className={classes.btn} onClick={toggleNav}>MENU</button>
       <ul className={classes.list}>
         <li>
           <Link href="/">Home</Link>
